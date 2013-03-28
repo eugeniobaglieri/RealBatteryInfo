c776800f623739493cfdbe10c54b6594cab7ca5a
Eugenio Baglieri <eugenio@iGenioMac.local>
2013-03-28 13:29:19 +0100

    Initial Commit

---
+++ RealBattery/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RealBattery
+//
+//  Created by Eugenio Baglieri on 28/03/13.
+//  Copyright (c) 2013 Eugenio Baglieri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
